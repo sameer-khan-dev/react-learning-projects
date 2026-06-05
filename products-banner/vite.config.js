44422742ecf07942c7cfbe16559474ee0c8bd7a2
Sameer Khan <252484667+sameer-khan-dev@users.noreply.github.com>
2026-06-05 23:50:00 +0530

    Add base & deploy via gh-pages

--- products-banner/vite.config.js
+++ products-banner/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/react-learning-projects/products-banner/",
   plugins: [react()],
 })
