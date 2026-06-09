a1fb4576cc0dd8e995342a7bafef27af120d2872
Sameer Khan <252484667+sameer-khan-dev@users.noreply.github.com>
2026-06-09 19:06:59 +0530

    Add base in like-button-and-counter/vite.config.js

--- like-button-and-counter/vite.config.js
+++ like-button-and-counter/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/react-learning-projects/like-button-and-counter/",
+  plugins: [react()],
 })
